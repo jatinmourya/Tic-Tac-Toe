e4d43d8145f45896ad20cafb8553dcc1809b343e
Jatin mourya <jatinmourya07798@gmail.com>
2022-02-04 11:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.querySelectorAll(".boxes").forEach(function (box) {
-  box.addEventListener("click", change);
-  counter = 1;
-  var player = document.getElementById("player");
-  player.innerHTML = "player 1 its your turn";
-  function change() {
-    if (this.style.backgroundImage == "") {
-      if (counter % 2 == 0) {
-        this.style.backgroundImage = "url('images/ooo.png')";
+        box.addEventListener("click", change);
+        counter = 1;
+        var player = document.getElementById("player");
         player.innerHTML = "player 1 its your turn";
-      } else {
-        this.style.backgroundImage = "url('images/xxx.png')";
-        player.innerHTML = "player 2 its your turn";
-      }
-      counter += 1;
-    }
-  }
-});
-function resetbtn() {
-  location.reload();
+        function change() {
+          if (this.style.backgroundImage == "") {
+            if (counter % 2 == 0) {
+              this.style.backgroundImage = "url('images/ooo.png')";
+              player.innerHTML = "player 1 its your turn";
+            } else {
+              this.style.backgroundImage = "url('images/xxx.png')";
+              player.innerHTML = "player 2 its your turn";
+            }
+            counter += 1;
+          }
+        }
+      });
+      function resetbtn() {
+        location.reload();
 }
