100f2477050ff711624878ef16e777890fc7fc63
Jatin mourya <jatinmourya07798@gmail.com>
2022-02-04 11:06:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll(".boxes").forEach(function (box) {
+  box.addEventListener("click", change);
+  counter = 1;
+  var player = document.getElementById("player");
+  player.innerHTML = "player 1 its your turn";
+  function change() {
+    if (this.style.backgroundImage == "") {
+      if (counter % 2 == 0) {
+        this.style.backgroundImage = "url('ooo.png')";
+        player.innerHTML = "player 1 its your turn";
+      } else {
+        this.style.backgroundImage = "url('xxx.png')";
+        player.innerHTML = "player 2 its your turn";
+      }
+      counter += 1;
+    }
+  }
+});
+function resetbtn() {
+  location.reload();
+}
